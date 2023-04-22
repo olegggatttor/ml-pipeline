910dfa71675b4bba21e42a0b66c6a4264aa784a8
oleg.bobrov <oleg.bobrov@corp.mail.ru>
2023-04-22 12:22:09 +0300

    - replace all with allclose

--- src/predict.py
+++ src/predict.py
@@ -14,7 +14,7 @@ def main():
         trainer = Trainer.from_pretrained(pickle.load(f), args.data, args.data)
         test_predictions = trainer.predict(trainer.get_test())
 
-        assert np.all(test_predictions == trainer.get_test()['count'])
+        assert np.allclose(test_predictions, trainer.get_test()['count'], rtol=0, atol=3)
 
 
 if __name__ == '__main__':
